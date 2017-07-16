a37cdb8d802c22521274e0bcb929ababa5ebb9cf
HopeMorehead <splendidwonder@gmail.com>
2017-07-16 14:36:19 -0400

    methods

---
+++ BankAccountProject1/Account.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountProject1
+{
+    class Account
+    {
+        decimal currentBalance = 100.00M;
+        decimal currentCheckingBalance;
+        decimal currentSavingsBalance;
+
+
+        //fields
+        private string name;
+        private int accountNumber;
+        private decimal checkings;
+        private decimal savings;
+        
+
+        //properties
+        public string Name { get; }
+        public int AccountNumber { get; }
+        public decimal Checking { get; set; }
+        public decimal Savings { get; set; }
+
+
+
+
+        public Account()
+        {
+            //default constructor
+        }
+
+
+        //custructor
+        public Account(string name, int accountNumber, decimal checkings, decimal savings)
+        {
+            this.name = name;
+            this.accountNumber = accountNumber;
+            this.checkings = checkings;
+            this.savings = savings;
+        }
+
+
+
+        //methods                      //temporary variables decimals
+        public  decimal deposit(decimal currentBalance,decimal depositAmount)
+        {
+            Console.WriteLine("How much would you like to deposit.");
+           depositAmount = decimal.Parse(Console.ReadLine());
+           decimal depositBalance = currentBalance + depositAmount;
+           return depositBalance;
+        }
+
+
+
+
+
+
+        //temporary variables decimals
+       
+        public decimal Withdrawal(decimal currentBalance,decimal withdrawalAmount)
+        {
+            Console.WriteLine("How much would you like to Withdrawal.");
+            withdrawalAmount = decimal.Parse(Console.ReadLine());
+            if (currentBalance <= 0.00M)
+            {
+                Console.WriteLine("Insufficient funds");              
+            }
+            decimal withdrawalBalance = currentBalance - withdrawalAmount;
+            return withdrawalBalance;
+
+
+
+        }
+
+
+
+
+        public  decimal ViewCurrentBalance(decimal currentBalance)
+        {
+           // currentCheckingBalance = currentBalance;
+            return currentBalance;
+        }
+
+
+
+
+
+     /*   public  decimal viewCurrentSavingsBalance(decimal currentSavingsBalance)
+        {
+            currentSavingsBalance = currentBalance;
+            return currentSavingsBalance;
+        }*/
+    }
+}
--- BankAccountProject1/Program.cs
+++ BankAccountProject1/Program.cs
@@ -10,6 +10,32 @@ namespace BankAccountProject1
     {
         static void Main(string[] args)
         {
+
+
+
+            decimal currentBalance = 100.00M;
+            decimal depositAmount = 0.00M;
+            decimal withdrawalAmount = 0.00M;
+
+
+            //can't seem to call return statement.Had to use a writeline
+            Account alex = new Account();
+            decimal depositBalance = alex.deposit(currentBalance, depositAmount);
+            Console.WriteLine(depositBalance);
+
+
+
+           decimal withdrawalBalance = alex.Withdrawal(depositBalance, withdrawalAmount);
+            Console.WriteLine(withdrawalBalance);
+
+
+
         }
+
+
+
+
+
     }
+
 }
